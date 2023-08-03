7358deefc20c715f2f5d38d29af6105d8f38718f
ridwanray <alabarise@gmail.com>
2023-08-03 13:51:06 +0100

    ccomplex num

---
+++ number_08.py
@@ -0,0 +1,23 @@
+# a = 10.0
+# b = 25.5
+
+# print(type(a))
+# print(type(b))
+
+# x = 10000000000000000000000000000.5
+# print(x)
+# complex_num = 2 + 4j
+# print(complex_num)
+# print(complex_num.real)
+# print(complex_num.imag)
+# print(complex_num.conjugate())
+
+
+
+
+
+
+
+
+
+
