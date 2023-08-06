09d979ec896b1a61f854dca474ddedce45c40e80
ridwanray <alabarise@gmail.com>
2023-08-06 09:08:47 +0100

    python numbers 08

--- number_08.py
+++ number_08.py
@@ -1,6 +1,8 @@
 # a = 10.0
 # b = 25.5
 
+
+
 # print(type(a))
 # print(type(b))
 
@@ -18,6 +20,41 @@
 
 
 
+
+
+
+
+# summation = 5 + 5
+# print(summation)
+# a = 5
+# b = 10.5
+# summation = a + b
+# print(summation)
+# print(type(summation))
+# a = 10.0
+# b = 5
+# diff = a - b
+# print(diff)
+# print(type(diff))
+# a = 20
+# b = 0
+# c = a / b
+# print(c)
+
+# exp = 5 ** 3.0
+# #5 * 5 * 5
+# print(exp)
+
+# mod =  5 % 2
+# print(mod)
+# Order of precedence
+# Parentheses () 
+# Exponentiation ** 
+# Multiplication:*, Division:/,Floor Division://, Modulo: %
+# Addition: +, Subtraction: 
+
+# expression =  3 * (2 - 1)
+# print(expression)
 
 
 
