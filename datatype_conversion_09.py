b564846fd90b348ed0256a800e2def1d4f2ad4e1
ridwanray <alabarise@gmail.com>
2023-08-09 18:55:30 +0100

    add lesson 9

---
+++ datatype_conversion_09.py
@@ -0,0 +1,47 @@
+# a = 10
+# b = int('5')
+# c = a + b 
+# print(c)
+# floating_num = int(20.0)
+# print(floating_num)
+# print(type(floating_num))
+
+# num1 = int(20.9)
+# num2 = int(20.1)
+# print(num1)
+# print(num2)
+
+# num3 = int('20.5')
+# print(num3)
+# float()
+# a = float('20')
+# b = float('20.5')
+# c = float(20)
+
+# print(a)
+# print(b)
+# print(c)
+
+# a = int('3')
+
+# msg = 'The book costs $' + str(5)
+# print(msg)
+a = str(20)
+b = str(20.9)
+print(type(a))
+print(type(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
