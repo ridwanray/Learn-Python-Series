b3a7904d46d76e57004aac5b933b7560d4eb2f47
ridwanray <alabarise@gmail.com>
2023-08-13 11:47:47 +0100

    Add math functions

---
+++ math_functions_10.py
@@ -0,0 +1,53 @@
+# abs_value = abs(-4)
+# print(abs_value)
+
+# 2⁴ : 2**4
+# exp = pow(2, 4)
+# print(exp)
+
+# max()
+# maximum =  max(90,4,5,6,9)
+# print(maximum)
+
+# min()
+# minimum = min(0,10,50,5)
+# print(minimum)
+# sqrt()
+# import math
+# square_root = math.sqrt(16)
+# print(square_root)
+# round()
+# round_num = round(1.8)
+# print(round_num)
+# num1 = round(22.44, 1)
+# num2 = round(22.48, 1)
+# num3 = round(22.438, 2)
+# print(num1)
+# print(num2)
+# print(num3)
+num1 = round(6.5)
+num2 = round(7.5)
+print(num1)
+print(num2)
+#4.5, 4.85, 6.485
+#4.7, 5.2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
