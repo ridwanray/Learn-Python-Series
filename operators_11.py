6c148ceffd2197b2fde01e356a162a8ec4575e96
ridwanray <alabarise@gmail.com>
2023-08-19 12:07:19 +0100

    add operators

---
+++ operators_11.py
@@ -0,0 +1,58 @@
+# name = 'Ali'
+# age = 10
+# print(name)
+# a = 10
+# b = 5
+# c = 20
+# c += b # c = c + b
+# c -= b # c = c - b
+# print(c)
+# result = 2 >= 1
+# print(result)
+
+# result = 5 <= 5
+# print(result)
+
+# result = 5 != 5
+# print(result)
+
+# result = 5 > 4 and 7 > 6 and 1 > 2
+# result = not (5 > 2 or 1 > 5)
+# result = not 5 > 4
+# print(result)
+#()
+# <, <=, ==, >=, >
+# not
+# and
+# or
+result1 = 5 < 10 or 10 < 15 and 15 > 5
+#           True or True and True
+#           True or True
+print(result1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
