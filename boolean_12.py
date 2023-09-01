9932bddd215d3438f5c2e89d7ec5f7ab928a7397
ridwanray <alabarise@gmail.com>
2023-09-01 12:24:48 +0100

    add List Data Type

---
+++ boolean_12.py
@@ -0,0 +1,35 @@
+# is_active = True
+# is_graduate = False
+# print(type(is_active))
+# print(type(is_graduate))
+# is_active = 6 > 5
+# print(type(is_active))
+# starts_with_r = "Ridwan".startswith("R")
+# print(starts_with_r)
+# print(type(starts_with_r))
+# bool()
+# x = bool(True)
+# y = bool(False)
+# print(x)
+# print(y)
+# print(bool(5))
+# print(bool(-5))
+# print(bool(0))
+
+# x = bool([1, 2])
+# y = bool([])
+# print(x)
+# print(y)
+# Arithmetic Operators
+result = True * False
+print(result)
+
+
+
+
+
+
+
+
+
+
---
+++ list_data_type_13.py
@@ -0,0 +1,68 @@
+# users = ["Ray", "Ali", "Alex"]
+
+# items = ["Ray", 9, True]
+
+# print(type(users))
+# print(type(items))
+# my_list = []
+# print(type(my_list))
+# list_a = list()
+# print(type(list_a))
+
+# names = ["Ali", "Alex", "Bob"]
+# print(names[-1])
+# print(names[-2])
+# print(names[-3])
+# numbers = [0,1,2,3,4,5]
+# is_present = 2 in numbers
+# print(is_present)
+# numbers = [5,6,7,8]
+# numbers[4] = 9
+# print(numbers)
+
+# # list_a = [1, 2] + [4, 5]
+# # print(list_a)
+# x = [1,2] * 2
+# print(x)
+# :
+# my_list = ["R", "i", "d", "w", "a", "n"]
+# # sliced_list = my_list[2:5]
+# # print(sliced_list)
+# # print(my_list[2:])
+# # print(my_list[:4])
+# print(my_list[:])
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# sliced_list = my_list[1::2]
+# print(sliced_list)
+reversed_list = my_list[::-1]
+print(reversed_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
