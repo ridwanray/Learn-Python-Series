627a6334b910d691cb31eb89666bc27b94e5b978
ridwanray <alabarise@gmail.com>
2023-09-17 16:41:27 +0100

    add tuple data type

---
+++ tuple_datatype.py
@@ -0,0 +1,89 @@
+# my_tuple = (1, 2, 3)
+# my_tuple_b = ('Ray', 786, 2.23, True)
+# print(type(my_tuple))
+# print(type(my_tuple_b))
+# empty_tuple = () #[]
+# empty_tuple_b = tuple() #list()
+# print(type(empty_tuple))
+# print(type(empty_tuple_b))
+# my_tuple  =  ("Ray", 7, 2.34, True)
+# print(my_tuple[4])
+# my_tuple  =  ("Ray", 7, 2.34, True)
+# my_tuple[2] = 7
+# my_tuple  =  ("Ray", 7, 2.34, True)
+# del my_tuple[1]
+# Tuple Concatenation +
+# my_tuple = (1, 2) + (3, 4)
+# print(my_tuple)
+# Tuple Repetition
+# my_tuple = (0, 1, 2) * 2
+# print(my_tuple)
+# my_tuple = (1, 2, 3, (1, 2), "Ray")
+# size = len(my_tuple)
+# print(size)
+# my_tuple = (1, 2, 3, 9, 12)
+# maximum = max(my_tuple)
+# print(maximum)
+
+my_tuple = (1, 2, 3, 9, 12)
+minimum = min(my_tuple)
+print(minimum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
