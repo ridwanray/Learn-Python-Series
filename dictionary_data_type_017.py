f9fda7a427fa6e8331867af498e10f92ed0de861
ridwanray <alabarise@gmail.com>
2023-10-26 08:05:38 +0100

    update code

---
+++ dictionary_data_type_017.py
@@ -0,0 +1,119 @@
+# user_info = {"name":"Ray", "name":"Bob", "age": 10}
+# print(user_info)
+# print(type(user_info))
+# var_a = {}
+# var_b = dict()
+# print(var_a)
+# print(var_b)
+# user_info = {"name":"Ray", "age":10, "Level": 9}
+# # print(user_info["name"])
+# print(user_info["age1"])
+# user_info = {"name":"Ray"}
+# user_info["age"] = 9
+# user_info["name"] = "Bob"
+# print(user_info)
+# user_info = {"name":"Ray", "age":10}
+# del user_info["name"]
+# print(user_info)
+# Merging dicts  a | b}
+# final_dict =  {"a": 1} | {"b" : 2, "a": 5}
+# print(final_dict)
+# in keyword
+# user_info =  {"name": "Ray"}
+# has_key = "name" in user_info
+# print(has_key)
+# len()
+# user_info =  {"name": "Ray", "age":10}
+# total_items = len(user_info)
+# print(total_items)
+
+# dict.get()
+# user_info = {"age":10, "name":"Ray"}
+# value = user_info.get('age1',"NA")
+# print(value)
+# dict.clear()
+# user_info =  {"age": 10}
+# user_info.clear()
+# print(user_info)
+# dict.pop()
+# user_info =  {"age": 10, "name": "Ray"}
+# removed_item = user_info.pop("age1")
+# print(removed_item)
+# print(user_info)
+# dict.update()
+# user_info = {"age": 7}
+# # user_info.update({"name":"Ray"})
+# user_info.update(name = "Ray")
+# print(user_info)
+# dict.items() dict.keys() dict.values()
+user_info = {"age": 10, "name": "Ray", "level" : 7}
+# print(user_info.items())
+# print(user_info.keys())
+print(user_info.values())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ set_data_type.py
@@ -0,0 +1,205 @@
+set_a = {5, 6, 7, 8, 5, 6, 7}
+# print(set_a)
+# print(type(set_a))
+
+# set(iterable)
+# set_b = set("abccd")
+# print(set_b)
+# print(type(set_b))
+
+# Empty set
+# empty_set = {}
+# print(empty_set)
+# print(type(empty_set))
+# Adding element to a set
+# set_a = {1, 2}
+# set_a.add(4)
+# print(set_a)
+# set.update()
+# initial_set = {1, 2}
+# initial_set.update("abc")
+# print(initial_set)
+# set.discard()
+# set_a = {1, 2, 4}
+# set_a.discard(2)
+# print(set_a)
+# set.clear()
+# set_a = {5, 6}
+# set_a.clear()
+# print(set_a)
+# set.pop()
+# set_a = {}
+# set_a.pop()
+# print(set_a)
+# sorted()
+# set_b = {9, 3, 6, 10, 0}
+# sorted_values = sorted(set_b)
+# print(sorted_values)
+# print(type(sorted_values))
+# set(): can be used to remove duplicates
+# duplicate_list = [1, 1, 2, 3, 1, 7]
+# set_values = set(duplicate_list)
+# print(set_values)
+# final_list = list(set_values)
+# print(final_list)
+# # len()
+# set_a = {1, 2, 4}
+# print(len(set_a))
+# # max()
+# set_a = {1, 5, 9}
+# max_value = max(set_a)
+# print(max_value)
+
+# min()
+# set_a = {1, 2, 5}
+# min_value = min(set_a)
+# print(min_value)
+# sum()
+# set_a = {1, 2, 3}
+# total = sum(set_a)
+# print(total)
+# Union of two sets |, a.union(b)
+# a = {0, 1, 2}
+# b = {3, 4, 5}
+# # c = a | b
+# c = a.union(b)
+# print(c)
+# Intersect of two set: &, a.intersection(b)
+# a = {1, 2, 3}
+# b = {2, 9, 10}
+# # c = a & b
+# c = a.intersection(b)
+# print(c)
+# Set difference: -
+# a = {1, 2, 3, 4}
+# b = {1, 2, 3}
+# c = b - a
+# print(c)
+# set.isdijoint(): a.disjoint(b)
+# a = {1, 2, 3}
+# b = {3, 2, 1}
+# c = a.isdisjoint(b)
+# print(c)
+# Membership check: in keyword
+set_a = {1, 2, 4}
+check = 0 in set_a
+print(check)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
