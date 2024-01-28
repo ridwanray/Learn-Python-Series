a31534679083c0188427c986e3e35310c843322b
ridwanray <alabarise@gmail.com>
2024-01-28 10:23:18 +0100

    added loops

---
+++ loop_31.py
@@ -0,0 +1,89 @@
+# for-loop
+# names = ["Ray", "Ali", "Bob", "Kay" ]
+
+# for name in names:
+#     print("Hi", name)
+# range(number)
+# for i in range(5):
+#     print(i)
+# letters = ["A", "B", "C", "D"]
+# # for index in range(len(letters)):
+# #     print(f'index:{index} item: {letters[index]}')
+# for index, item in enumerate(letters):
+#     print(f"{index}: {item}")
+# for-loop on dict
+# my_dict =  {"name":"Ray", "age": 10}
+# for key, item in my_dict.items():
+#     print(key, item)
+# break
+# for i in range(6):
+#     if i == 4:
+#         break
+#     print(i)
+    
+# print("Done!")
+# continue in loops
+
+# for i in range(8):
+#     if i == 4:
+#         continue
+#     print(i)
+# while loop
+
+# while <expr>:
+#     <loop body>
+# x = [1, 2]
+# while x:
+#     print("Hi")
+# nums = [1, 5, 6, 8]
+# while nums:
+#     print(nums.pop(-1))
+    
+# print("Done!")
+store = []
+
+# while True:
+#     user_input = input("What do you want to do? \nEnter N to quit:")
+#     if user_input == "N":
+#         break
+#     store.append(user_input)
+# print(store)
+# else clause
+# while <expr>:
+#     <statement(s)>
+#<additional_statement(s)>
+nums = [1, 2]
+
+while nums:
+        print(nums.pop(-1))
+else:
+        print("done")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
